cc4cfed6d9b24ce585cd1c416cace913915b93a1
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2022-11-16 07:21:51 -0800

    add .

---
+++ hr_hospital3/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': "Hospital",
+
+    'summary': """
+        Module hospital. Data clients.
+    """,
+
+    'license': 'OPL-1',
+
+    'author': "grigoriykosarev@gmail.com",
+    'website': "http://www.grigoriy-kosarev.com",
+
+    'category': 'Uncategorized',
+    'version': '15.0.1.0.0',
+
+    'depends': [
+        'base',
+    ],
+
+    'external_dependencies': {'python': [], },
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hs3_hospital_menu.xml',
+        'views/patient_views.xml',
+        'views/doctor_views.xml',
+        'views/diagnosis_views.xml',
+    ],
+
+    'demo': [
+    ],
+}
