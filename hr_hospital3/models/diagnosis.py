1a0a8c0db769908cd0bc2bef7e78d2e4466f0d3d
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2022-11-29 08:29:54 -0800

    ok

--- hr_hospital3/models/diagnosis.py
+++ hr_hospital3/models/diagnosis.py
@@ -4,5 +4,6 @@ class Diagnosis(models.Model):
     _name = 'hs3.diagnosis'
     _description = 'Diagnosis'
 
+    doctor = fields.Char(required=True)
     name = fields.Char(required=True)
     diseases = fields.Char(required=True)
--- hr_hospital3/models/patient.py
+++ hr_hospital3/models/patient.py
@@ -4,13 +4,14 @@ class Patient(models.Model):
     _name = 'hs3.patient'
     _description = 'Patient'
 
-    full_name = fields.Char(required=True)
+    full_name = fields.Char(required=True, string='Full name')
     gender = fields.Selection(
         default='male',
+        required=True,
         selection= [('male', _('Male')), ('female', _('Female'))]
     )
-    birthday = fields.Date(string='Date of birth')
+    birthday_date = fields.Date(string='Date of birth', required=True)
     age = fields.Integer(default='18')
     passport = fields.Char()
-    contact_person = fields.Char()
+    contact_person = fields.Char(string='Contact person')
 
